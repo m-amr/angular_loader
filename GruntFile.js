570c9d55490d9e20e27634224bed6ef315153915
mohamed amr <m_ma7med@hotmail.com>
2015-11-06 16:54:00 +0200

    add GruntFile.js

---
+++ GruntFile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by amr on 11/6/15.
+ */
+
+module.exports = function(grunt){
+
+    grunt.registerTask('build', function(){
+        console.log('building task is running....');
+    });
+
+};
