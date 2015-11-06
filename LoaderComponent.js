cf5b9869304e909c47931d02871aef3a9e22ec8e
mohamed amr <m_ma7med@hotmail.com>
2015-11-06 15:41:43 +0200

    define loaderHttpInterceptor to $broadcast event before sending the request and on handle the response

--- LoaderComponent.js
+++ LoaderComponent.js
@@ -6,3 +6,36 @@
 
 // 1 - define module.
 var loaderModule = angular.module('loader-component', []);
+
+// 2- define provider
+loaderModule.factory('loaderHttpInterceptor', ['$q', '$rootScope', function($q, $rooScope) {
+    return {
+        // optional method
+        'request': function(config) {
+            $rooScope.$broadcast('loading:start');
+            return config;
+        },
+
+        // optional method
+        'requestError': function(rejection) {
+
+            return $q.reject(rejection);
+        },
+
+
+
+        // optional method
+        'response': function(response) {
+            // do something on success
+            $rooScope.$broadcast('loading:finish');
+            return response;
+        },
+
+        // optional method
+        'responseError': function(rejection) {
+            // do something on error
+            $rooScope.$broadcast('loading:finish');
+            return $q.reject(rejection);
+        }
+    };
+}]);
